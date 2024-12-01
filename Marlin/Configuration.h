4b0ad33b32d98a159075d303e3ceaf80c987f4cb
Felix Collin <felix.collin@outlook.com>
2024-12-01 15:00:57 -0500

    updated calib of steps w new PLA raft config

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -746,7 +746,7 @@
  * Override with M92
  *                                      X, Y, Z, E0 [, E1[, E2...]]
  */
-#define DEFAULT_AXIS_STEPS_PER_UNIT   { 80.33, 79.62, 1651.14, 139.04 }
+#define DEFAULT_AXIS_STEPS_PER_UNIT   { 80.33, 79.15, 1587.63, 139.04 }
 
 /**
  * Default Max Feed Rate (mm/s)
