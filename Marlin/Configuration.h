85608fb6e2eb61be3ff725be4a8ce9ed7227ad12
Felix Collin <felix.collin@outlook.com>
2024-12-24 12:02:06 -0500

    Updated steps per unit per latest calib.

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1200,7 +1200,7 @@
  * Override with M92
  *                                      X, Y, Z [, I [, J [, K...]]], E0 [, E1[, E2...]]
  */
-#define DEFAULT_AXIS_STEPS_PER_UNIT   { 80.73, 78.95, 1611.81, 139.04 }
+#define DEFAULT_AXIS_STEPS_PER_UNIT   { 81.75, 78.36, 1620.72, 139.04 }
 
 /**
  * Default Max Feed Rate (linear=mm/s, rotational=°/s)
